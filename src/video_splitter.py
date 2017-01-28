8f204c0594097fc4650d4c9c88486c83001fc3f2
Alexandru-Paul Copil <copil.alexander@gmail.com>
2017-01-28 22:11:05 +0000

    Update and fix video splitter

--- src/video_splitter.py
+++ src/video_splitter.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-import dirconf
+import subprocess
 sys.path.append('/usr/local/lib/python2.7/site-packages')
 import cv2
 start = 0
@@ -14,21 +14,27 @@ increment = end
 vidcap = cv2.VideoCapture(sys.argv[1])
 success,image = vidcap.read()
 dir_path = ("/data")
+
+
 while success:
 	frame_count = str(end/1000)
 	success,image = vidcap.read()
-	vidcap.set(start, end)
-	output = cv2.imwrite(dirconf.IMAGES + "/000" + frame_count + ".jpg", image)
-	start = end
+	print "##############################"
+	print "Start duration: " + str(start)
+	print "End duration: " + str(end)
+	print "##############################"
+	vidcap.set(0, end)
+	output = cv2.imwrite("data/" + "000" + frame_count + ".jpg", image)
 	end = end + increment
- # 	if (end >= 25000):
- # 		break
+	command = " ffmpeg -i " + sys.argv[1] + " -f segment -strftime 1 -segment_time 5 -segment_format wav " + "data/frame_" + frame_count + ".wav"
 
-# import subprocess
-#
-# command = "ffmpeg -ss " + sys.argv[1] + " -ab 160k -ac 2 -ar 44100 -vn movie.wav"
-#
-# subprocess.call(command, shell=True)
+	subprocess.call(command, shell=True)
+ 	#if (end >= 40000):
+ 		#break
+
+
+
+
 
 
 '''import http.client, urllib.parse, json
