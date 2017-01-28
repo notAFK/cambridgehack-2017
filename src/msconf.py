02de5e5af11a0399e053061ff86323e8584da6f8
Alexandru-Paul Copil <copil.alexander@gmail.com>
2017-01-28 16:11:49 +0000

    Started indexer and uplink

--- src/msconf.py
+++ src/msconf.py
@@ -1,1 +1,7 @@
-API_KEY = ''
+API_KEY = '5f93d9702e764cc6ab353360474e565f'
+
+HEADER = {'Ocp-Apim-Subscription-Key': API_KEY,
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'}
+
+TEST_URL = 'https://thumbs.dreamstime.com/z/young-people-13305611.jpg'
