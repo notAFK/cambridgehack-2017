6ad7cf6dab831bc13ac4a27c92fff89160e9ba0f
Alex Radu <dragosthealx@gmail.com>
2017-01-29 10:46:06 +0000

    fixed analysis stuff

--- src/analysis.py
+++ src/analysis.py
@@ -1,6 +1,10 @@
 import json
+import dirconf
 import collections
-with open('summary.json', 'r') as data_file:    
+
+import sys
+
+with open(dirconf.DATA + '/summary.json', 'r') as data_file:
     data = json.load(data_file)
 
 maxfaces = 0
@@ -8,12 +12,16 @@ maxfaces = 0
 for frame in data:
     if maxfaces < len(frame["faces"]):
         maxfaces = len(frame["faces"])
-        
+
+
 frames = []
 infoPerFrame = {}
 videoEmotions = {"videoSadness": 0, "videoNeutral": 0, "videoContempt": 0,
                  "videoDisgust": 0, "videoAnger": 0, "videoSurprise": 0,
                  "videoFear": 0, "videoHappiness": 0}
+analyticsKeys = ["attentionIndex", "averageSadness", "averageNeutral",
+                 "averageContempt", "averageDisgust", "averageAnger",
+                 "averageSurprise", "averageFear", "averageHappiness"]
 videoDisgust = 0
 videoFear = 0
 videoSurprise = 0
@@ -25,205 +33,98 @@ videoAnger = 0
 
 i = 0
 
-if len(data[0]["faces"])==0:
-    frames.append({"frame": data[0]["frame"]})
-    infoPerFrame.update({"attentionIndex": 0, "averageSadness": 0, "averageNeutral": 0,
-                         "averageContempt": 0, "averageDisgust": 0, "averageAnger": 0,
-                         "averageSurprise": 0, "averageFear": 0, "averageHappiness": 0})
-    frames[i].update(infoPerFrame)
+frames.append({"frame": data[0]["frame"], "info": {}})
+if len(data[0]["faces"]) == 0:
+    infoPerFrame = {}
+    for key in analyticsKeys:
+      infoPerFrame[key] = 0
+    frames[0] = infoPerFrame
     i = i + 1
 
-    for frame in data[1:]:
-        
-        totalSadness = 0
-        totalNeutral = 0
-        totalContempt = 0
-        totalDisgust = 0
-        totalAnger = 0
-        totalSurprise = 0
-        totalFear = 0
-        totalHappiness = 0
-
-        frames.append({"frame": frame["frame"]})
-        if len(frame["faces"])!=0:
-            infoPerFrame.update({"attentionIndex": len(frame["faces"])/float(maxfaces)})
-            for face in frame["faces"]:
-                totalSadness += face["scores"]["sadness"]
-                totalNeutral += face["scores"]["neutral"]
-                totalContempt += face["scores"]["contempt"]
-                totalDisgust += face["scores"]["disgust"]
-                totalAnger += face["scores"]["anger"]
-                totalSurprise += face["scores"]["surprise"]
-                totalFear += face["scores"]["fear"]
-                totalHappiness += face["scores"]["happiness"]
-        else:
-            prevIndex = data.index(frame)-1
-            infoPerFrame.update({"attentionIndex": len(data[prevIndex]["faces"])/float(maxfaces)/2.0})
-            
-            for face in data[prevIndex]["faces"]:
-                totalSadness += face["scores"]["sadness"]/2.0
-                totalNeutral += face["scores"]["neutral"]/2.0
-                totalContempt += face["scores"]["contempt"]/2.0
-                totalDisgust += face["scores"]["disgust"]/2.0
-                totalAnger += face["scores"]["anger"]/2.0
-                totalSurprise += face["scores"]["surprise"]/2.0
-                totalFear += face["scores"]["fear"]/2.0
-                totalHappiness += face["scores"]["happiness"]/2.0
-
-        emotions={}
-        if len(frame["faces"])==0 & len(data[prevIndex]["faces"])==0:
-            emotions["averageSadness"] = 0
-            emotions["averageNeutral"] = 0
-            emotions["averageContempt"] = 0
-            emotions["averageDisgust"] = 0
-            emotions["averageAnger"] = 0
-            emotions["averageSurprise"] = 0
-            emotions["averageFear"] = 0
-            emotions["averageHappiness"] = 0
-        elif len(frame["faces"])!=0:
-            length = len(frame["faces"])
-            emotions["averageSadness"] = format(totalSadness/float(length),'.15f')
-            videoSadness += float(emotions["averageSadness"])
-            emotions["averageNeutral"] = format(totalNeutral/float(length),'.15f')
-            videoNeutral += float(emotions["averageNeutral"])
-            emotions["averageContempt"] = format(totalContempt/float(length),'.15f')
-            videoContempt += float(emotions["averageContempt"])
-            emotions["averageDisgust"] = format(totalDisgust/float(length),'.15f')
-            videoDisgust += float(emotions["averageDisgust"])
-            emotions["averageAnger"] = format(totalAnger/float(length),'.15f')
-            videoAnger += float(emotions["averageAnger"])
-            emotions["averageSurprise"] = format(totalSurprise/float(length),'.15f')
-            videoSurprise += float(emotions["averageSurprise"])
-            emotions["averageFear"] = format(totalFear/float(length),'.15f')
-            videoFear += float(emotions["averageFear"])
-            emotions["averageHappiness"] = format(totalHappiness/float(length),'.15f')
-            videoHappiness += float(emotions["averageHappiness"])
-        else:
-            length = len(data[prevIndex]["faces"])
-                
-            emotions["averageSadness"] = format(totalSadness/float(length)/2.0,'.15f')
-            videoSadness += float(emotions["averageSadness"])
-            emotions["averageNeutral"] = format(totalNeutral/float(length)/2.0,'.15f')
-            videoNeutral += float(emotions["averageNeutral"])
-            emotions["averageContempt"] = format(totalContempt/float(length)/2.0,'.15f')
-            videoContempt += float(emotions["averageContempt"])
-            emotions["averageDisgust"] = format(totalDisgust/float(length)/2.0,'.15f')
-            videoDisgust += float(emotions["averageDisgust"])
-            emotions["averageAnger"] = format(totalAnger/float(length)/2.0,'.15f')
-            videoAnger += float(emotions["averageAnger"])
-            emotions["averageSurprise"] = format(totalSurprise/float(length)/2.0,'.15f')
-            videoSurprise += float(emotions["averageSurprise"])
-            emotions["averageFear"] = format(totalFear/float(length)/2.0,'.15f')
-            videoFear += float(emotions["averageFear"])
-            emotions["averageHappiness"] = format(totalHappiness/float(length)/2.0,'.15f')
-            videoHappiness += float(emotions["averageHappiness"])
-            
-        infoPerFrame.update(emotions)       
-        frames[i].update(infoPerFrame)
-        i = i + 1
-
 else:
-    for frame in data:
-          
-        totalSadness = 0
-        totalNeutral = 0
-        totalContempt = 0
-        totalDisgust = 0
-        totalAnger = 0
-        totalSurprise = 0
-        totalFear = 0
-        totalHappiness = 0
+  totalSadness = 0
+  totalNeutral = 0
+  totalContempt = 0
+  totalDisgust = 0
+  totalAnger = 0
+  totalSurprise = 0
+  totalFear = 0
+  totalHappiness = 0
+  noFaces = len(data[0]["faces"])
+  for face in data[0]["faces"]:
+      totalSadness += face["scores"]["sadness"]
+      totalNeutral += face["scores"]["neutral"]
+      totalContempt += face["scores"]["contempt"]
+      totalDisgust += face["scores"]["disgust"]
+      totalAnger += face["scores"]["anger"]
+      totalSurprise += face["scores"]["surprise"]
+      totalFear += face["scores"]["fear"]
+      totalHappiness += face["scores"]["happiness"]
 
-        frames.append({"frame": frame["frame"]})
-        if len(frame["faces"])!=0:
-            infoPerFrame.update({"attentionIndex": len(frame["faces"])/float(maxfaces)})
-            for face in frame["faces"]:
-                totalSadness += face["scores"]["sadness"]
-                totalNeutral += face["scores"]["neutral"]
-                totalContempt += face["scores"]["contempt"]
-                totalDisgust += face["scores"]["disgust"]
-                totalAnger += face["scores"]["anger"]
-                totalSurprise += face["scores"]["surprise"]
-                totalFear += face["scores"]["fear"]
-                totalHappiness += face["scores"]["happiness"]
-        else:
-            prevIndex = data.index(frame)-1
-            infoPerFrame.update({"attentionIndex": len(data[prevIndex]["faces"])/float(maxfaces)/2})
-            
-            for face in data[prevIndex]["faces"]:
-                totalSadness += face["scores"]["sadness"]/2.0
-                totalNeutral += face["scores"]["neutral"]/2.0
-                totalContempt += face["scores"]["contempt"]/2.0
-                totalDisgust += face["scores"]["disgust"]/2.0
-                totalAnger += face["scores"]["anger"]/2.0
-                totalSurprise += face["scores"]["surprise"]/2.0
-                totalFear += face["scores"]["fear"]/2.0
-                totalHappiness += face["scores"]["happiness"]/2.0
+      infoPerFrame = {"attentionIndex": noFaces/float(maxfaces),
+                      "averageSadness": totalSadness/float(maxfaces),
+                      "averageNeutral": totalNeutral/float(maxfaces),
+                      "averageContempt": totalContempt/float(maxfaces),
+                      "averageDisgust": totalDisgust/float(maxfaces),
+                      "averageAnger": totalAnger/float(maxfaces),
+                      "averageSurprise": totalSurprise/float(maxfaces),
+                      "averageFear": totalFear/float(maxfaces),
+                      "averageHappiness": totalHappiness/float(maxfaces)}
+      frames[0]["info"] = infoPerFrame
 
-        emotions={}
-        if len(frame["faces"])==0 & len(data[prevIndex]["faces"])==0:
-            emotions["averageSadness"] = 0
-            emotions["averageNeutral"] = 0
-            emotions["averageContempt"] = 0
-            emotions["averageDisgust"] = 0
-            emotions["averageAnger"] = 0
-            emotions["averageSurprise"] = 0
-            emotions["averageFear"] = 0
-            emotions["averageHappiness"] = 0
-        elif len(frame["faces"])!=0:
-            length = len(frame["faces"])
-            emotions["averageSadness"] = format(totalSadness/float(length),'.15f')
-            videoSadness += float(emotions["averageSadness"])
-            emotions["averageNeutral"] = format(totalNeutral/float(length),'.15f')
-            videoNeutral += float(emotions["averageNeutral"])
-            emotions["averageContempt"] = format(totalContempt/float(length),'.15f')
-            videoContempt += float(emotions["averageContempt"])
-            emotions["averageDisgust"] = format(totalDisgust/float(length),'.15f')
-            videoDisgust += float(emotions["averageDisgust"])
-            emotions["averageAnger"] = format(totalAnger/float(length),'.15f')
-            videoAnger += float(emotions["averageAnger"])
-            emotions["averageSurprise"] = format(totalSurprise/float(length),'.15f')
-            videoSurprise += float(emotions["averageSurprise"])
-            emotions["averageFear"] = format(totalFear/float(length),'.15f')
-            videoFear += float(emotions["averageFear"])
-            emotions["averageHappiness"] = format(totalHappiness/float(length),'.15f')
-            videoHappiness += float(emotions["averageHappiness"])
-        else:
-            length = len(data[prevIndex]["faces"])
-                
-            emotions["averageSadness"] = format(totalSadness/float(length)/2.0,'.15f')
-            videoSadness += float(emotions["averageSadness"])
-            emotions["averageNeutral"] = format(totalNeutral/float(length)/2.0,'.15f')
-            videoNeutral += float(emotions["averageNeutral"])
-            emotions["averageContempt"] = format(totalContempt/float(length)/2.0,'.15f')
-            videoContempt += float(emotions["averageContempt"])
-            emotions["averageDisgust"] = format(totalDisgust/float(length)/2.0,'.15f')
-            videoDisgust += float(emotions["averageDisgust"])
-            emotions["averageAnger"] = format(totalAnger/float(length)/2.0,'.15f')
-            videoAnger += float(emotions["averageAnger"])
-            emotions["averageSurprise"] = format(totalSurprise/float(length)/2.0,'.15f')
-            videoSurprise += float(emotions["averageSurprise"])
-            emotions["averageFear"] = format(totalFear/float(length)/2.0,'.15f')
-            videoFear += float(emotions["averageFear"])
-            emotions["averageHappiness"] = format(totalHappiness/float(length)/2.0,'.15f')
-            videoHappiness += float(emotions["averageHappiness"])
+totalVideo = {}
+for key in analyticsKeys:
+  totalVideo[key] = 0
+
+for index,frame in enumerate(data[1:]):
+    totalSadness = 0
+    totalNeutral = 0
+    totalContempt = 0
+    totalDisgust = 0
+    totalAnger = 0
+    totalSurprise = 0
+    totalFear = 0
+    totalHappiness = 0
+    infoPerFrame = {}
+    for key in analyticsKeys:
+      infoPerFrame[key] = 0;
+    print frame
+    frames.append({"frame": frame["frame"], "info": infoPerFrame})
+    if len(frame["faces"])!=0:
+        noFaces = len(frame["faces"])
+        for face in frame["faces"]:
+            totalSadness += face["scores"]["sadness"]
+            totalNeutral += face["scores"]["neutral"]
+            totalContempt += face["scores"]["contempt"]
+            totalDisgust += face["scores"]["disgust"]
+            totalAnger += face["scores"]["anger"]
+            totalSurprise += face["scores"]["surprise"]
+            totalFear += face["scores"]["fear"]
+            totalHappiness += face["scores"]["happiness"]
+        infoPerFrame = {"attentionIndex": noFaces/float(maxfaces),
+                        "averageSadness": totalSadness/float(maxfaces),
+                        "averageNeutral": totalNeutral/float(maxfaces),
+                        "averageContempt": totalContempt/float(maxfaces),
+                        "averageDisgust": totalDisgust/float(maxfaces),
+                        "averageAnger": totalAnger/float(maxfaces),
+                        "averageSurprise": totalSurprise/float(maxfaces),
+                        "averageFear": totalFear/float(maxfaces),
+                        "averageHappiness": totalHappiness/float(maxfaces)}
+    else:
+        infoPerFrame = {}
+        for key in analyticsKeys:
+          infoPerFrame[key] = frames[index+1]["info"][key]/2.0
+
+    for key, emotion in infoPerFrame.iteritems():
+      totalVideo[key] += infoPerFrame[key]
+
+    frames[index+1]["info"] = infoPerFrame
+
+for key in analyticsKeys:
+  totalVideo[key] = totalVideo[key] / float(len(data))
+
+averages = {"averages": frames, "videoAverages": totalVideo}
 
 
-        infoPerFrame.update(emotions)       
-        frames[i].update(infoPerFrame)
-        i = i + 1
-
-videoEmotions["videoDisgust"] = videoDisgust/float(len(data))
-videoEmotions["videoFear"] = videoFear/float(len(data))
-videoEmotions["videoSurprise"] = videoSurprise/float(len(data))
-videoEmotions["videoSadness"] = videoSadness/float(len(data))
-videoEmotions["videoNeutral"] = videoNeutral/float(len(data))
-videoEmotions["videoHappiness"] = videoHappiness/float(len(data))
-videoEmotions["videoContempt"] = videoContempt/float(len(data))
-videoEmotions["videoAnger"] = videoAnger/float(len(data))
-
-averages = {"averages": frames, "videoAverages": videoEmotions}
-
-with open('averages.json', 'w+') as file:
+with open(dirconf.WEBSITE + '/' + sys.argv[1], 'w+') as file:
     json.dump(averages, file, indent=4)
-
