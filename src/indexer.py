c8ebf076827f6ea849a455c1416df5a48d8510cb
Alexandru-Paul Copil <copil.alexander@gmail.com>
2017-01-29 00:36:07 +0000

    Add mstext script multi tool

--- src/indexer.py
+++ src/indexer.py
@@ -2,21 +2,25 @@ import json
 import sys
 import os
 import dirconf
-import msaudio
 import msuplink
+import msaudio
+import mstext
+
 
 if __name__ == '__main__':
 
     image_list = []
-    text_list = []
     responses = []
 
     for image in os.listdir(dirconf.IMAGES):
         if image.endswith('.jpg'):
             image_list.append(image)
 
+    text_list = []
+    keyphrase_list = []
     for text in msaudio.translate_all():
-        text_list.append(text)
+        text_list.append(mstext.process_text(text, 'f003a623c8f246b783e1f9c7076e4372'))
+        keyphrase_list.append(mstext.process_keyphrases(text, 'f003a623c8f246b783e1f9c7076e4372'))
 
     print 'PROCESSING ' + str(len(image_list)) + ' IMAGES'
 
@@ -27,7 +31,7 @@ if __name__ == '__main__':
         if len(response) == 1:
             print 'ERROR'
         else:
-            responses.append({'frame': image[:5], 'faces': response, 'speech': text_list[i]})
+            responses.append({'frame': image[:5], 'faces': response, 'speech': text_list[i], 'keyphrases': keyphrase_list[i]})
 
     with open(os.path.join(dirconf.DATA, 'summary.json'), 'w+') as summary:
         json.dump(responses, summary, indent=4)
---
+++ src/mstext.py
@@ -0,0 +1,43 @@
+import json
+import requests
+
+
+# Create standard header for Microsoft Text An. API
+def write_header(key):
+    HEADER = {'Ocp-Apim-Subscription-Key': key,
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'}
+    return HEADER
+
+
+# Prepare the document files for upload.
+def write_doc_string(mytext):
+    # Standard data format for MS Text An. API request.
+    DATA = {
+     "documents": [
+         {
+             "language": "en",
+             "id": "1",
+             "text": mytext
+             }]}
+    return json.dumps(DATA)
+
+
+def process_text(mytext, apikey):
+    if not mytext:
+        return "null"
+    with requests.session() as shortname:
+        answer = shortname.post('https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment', headers=write_header(apikey), data=write_doc_string(mytext))
+    returndict = json.loads(answer.content)
+    print returndict
+    return returndict['documents'][0]['score']
+
+
+def process_keyphrases(mytext, apikey):
+    if not mytext:
+        return "null"
+    with requests.session() as shortname:
+        answer = shortname.post('https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases', headers=write_header(apikey), data=write_doc_string(mytext))
+    returndict = json.loads(answer.content)
+    print returndict
+    return returndict['documents'][0]['keyPhrases']
