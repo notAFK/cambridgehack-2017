26810dfb84d2e77a90c97f760ecbaa5b1e504dd1
Alexandru-Paul Copil <copil.alexander@gmail.com>
2017-01-29 07:40:47 +0000

    Hotfix graph size?

--- src/indexer.py
+++ src/indexer.py
@@ -16,6 +16,8 @@ if __name__ == '__main__':
         if image.endswith('.jpg'):
             image_list.append(image)
 
+    print "IMAGE LIST: ", image_list
+
     text_list = []
     keyphrase_list = []
     for text in msaudio.translate_all():
@@ -28,10 +30,15 @@ if __name__ == '__main__':
         response = msuplink.process_image(os.path.join(dirconf.IMAGES,
                                                        image))
 
+        if i >= len(text_list):
+            i = len(text_list)-1
+
         if len(response) == 1:
-            print 'ERROR'
+            responses.append({'frame': image.split('.')[0][1:], 'faces': [], 'speech': text_list[i], 'keyphrases': keyphrase_list[i]})
         else:
             responses.append({'frame': image.split('.')[0][1:], 'faces': response, 'speech': text_list[i], 'keyphrases': keyphrase_list[i]})
+
+    print 'RESPONSES: ' + str(len(responses))
 
     with open(os.path.join(dirconf.DATA, 'summary.json'), 'w+') as summary:
         json.dump(responses, summary, indent=4)
